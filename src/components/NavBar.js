0f18857aa4348705504a60395f9fa9dc27dffe3d
john-reiner <johnreiner1990@gmail.com>
2022-03-06 11:26:03 -0500

    navbar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,31 +4,31 @@ import {Container, Navbar, Nav} from 'react-bootstrap'
 
 export default function NavBar() {
     return (
-        <Navbar bg="primary" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
             <Container fluid>
                 <Navbar.Brand href="#home">
                 <Icon 
                     icon="gridicons:share-computer" 
-                    width="30"
                     width="30"
                     height="30"
                     className="d-inline-block align-top"
                 />{' '}
                 John Reiner
                 </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                    <Nav.Link href="#">Home</Nav.Link>
-                    <Nav.Link href="#">About</Nav.Link>
-                    <Nav.Link href="#">Skills</Nav.Link>
-                    <Nav.Link href="#">Resume</Nav.Link>
-                    <Nav.Link href="#">Projects</Nav.Link>
-                    <Nav.Link href="#">Contact</Nav.Link>
-                </Nav>
+                        <Nav.Link href="#">Home</Nav.Link>
+                        <Nav.Link href="#">About</Nav.Link>
+                        <Nav.Link href="#">Skills</Nav.Link>
+                        <Nav.Link href="#">Resume</Nav.Link>
+                        <Nav.Link href="#">Projects</Nav.Link>
+                        <Nav.Link href="#">Contact</Nav.Link>
+                    </Nav>
                 <Nav>
                     <Nav.Link href="#">
                         <Icon 
                             icon="fa:github-square" 
-                            width="30"
                             width="30"
                             height="30"
                             className="d-inline-block align-top"
@@ -38,12 +38,12 @@ export default function NavBar() {
                         <Icon 
                             icon="brandico:linkedin-rect" 
                             width="30"
-                            width="30"
                             height="30"
                             className="d-inline-block align-top"
                         />{' '}
                     </Nav.Link>
                 </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
