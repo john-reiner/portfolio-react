d4eae643b91b628333cd979198faf700a23c483d
john-reiner <johnreiner1990@gmail.com>
2022-03-08 13:23:30 -0500

    restyled about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,43 +1,57 @@
 import React from 'react'
+import '../styles/About.css'
 import {Col, Row, Container, Image} from 'react-bootstrap'
 import { Icon } from '@iconify/react';
 export default function About() {
   return (
-    <div id="about-container">
-      {/* <Container > */}
-        <Row id="about-row-one">
-          <Col>
-            <h2>About Me</h2>
-          </Col>
-        </Row>
-        <Row id="about-row-two">
-          <Col id="photo-container" md={4}>
-            <div id="photo">
-              <Image rounded src="https://via.placeholder.com/200x250" id="picture" />
-              <div id="photo-icons">
-                <Icon icon="logos:linkedin-icon" />
-                <Icon icon="fa:github-square" />
+    <div id="about-container" >
+      <Container  >
+        <Row id="about-row">
+          <Col id="about-row" md={4}>
+          <div className="window-container">
+            <div className="window-bar">
+              <div className="window-bar-buttons">
+                <div className="red-button"></div>
+                <div className="yellow-button"></div>
+                <div className="green-button"></div>
               </div>
+              <div className="window-bar-content">John Reiner</div>
             </div>
+            {/* content Start */}
+            <div id="about-photo-container">
+              <Image  src="https://via.placeholder.com/200x250" id="picture" />
+                <div id="photo-icons">
+                  <a href="https://www.linkedin.com/in/john-reiner/" target="_blank" rel="noreferrer"  className="social-icon">
+                      <Icon icon="fa:linkedin-square"  />
+                  </a>
+                  <a href="https://github.com/john-reiner" target="_blank" rel="noreferrer" className="social-icon" >
+                      <Icon icon="fa:github-square"  />
+                  </a>
+                </div>
+            </div>
+            {/* {Content finish} */}
+          </div>
           </Col>
           <Col md={8}>
-            <div id="about-text-container">
-              <div id="about-bar">
-                <div id="about-bar-buttons">
-                  <div id="red-button"></div>
-                  <div id="yellow-button"></div>
-                  <div id="green-button"></div>
+            <div className="window-container">
+              <div className="window-bar">
+                  <div className="window-bar-buttons">
+                  <div className="red-button"></div>
+                  <div className="yellow-button"></div>
+                  <div className="green-button"></div>
+                  </div>
+                  <div className="window-bar-content">Hi! 👋 </div>
+              </div>
+              {/* content Start */}
+                <div id="about-text-container">
+                  <p>I'm John.</p>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
-                <div id="about-bar-title"><b>Hi! 👋 </b></div>
-              </div>
-              <div id="about-content">
-                <p>I'm John.</p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              </div>
+              {/* {Content finish} */}
             </div>
           </Col>
         </Row>
-      {/* </Container> */}
-    </div>
+      </Container>
+    // </div>
   )
 }
