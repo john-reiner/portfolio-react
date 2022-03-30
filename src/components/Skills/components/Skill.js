17752c46e244d639615b490a70f08f36995a4dd1
john-reiner <johnreiner1990@gmail.com>
2022-03-30 05:59:51 -0400

    skills css fix

--- src/components/Skills/components/Skill.js
+++ src/components/Skills/components/Skill.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import '../styles/Skills.css'
-import {Row} from 'react-bootstrap'
 import { Icon } from '@iconify/react';
 
 export default function Skill(props) {
     return (
-        <Row className="skill">
-            <p>{props.name}</p>
-            {props.icon !== undefined ? <Icon icon={props.icon}/> : <Icon icon={"logos:" + props.name} />}
-        </Row>
+        <div className="skill">
+            <p className='skill-item'>{props.name}</p>
+            <span className='skill-item'>{props.icon !== undefined ? <Icon icon={props.icon}/> : <Icon icon={"logos:" + props.name} />}</span>
+        </div>
     )
 }
